ae830c23763b6d28bef9792ec1c0c1a58f7af826
IT logix <itlogix@ITs-MacBook-Pro.local>
2020-01-16 13:56:38 +0400

    do not allow change input type

---
+++ js/index.js
@@ -0,0 +1,11 @@
+// const input = document.querySelector('input[type=password]');
+
+// const observer = new MutationObserver(function(){
+//     if(input.type === "text") {
+//         input.type = "password";
+//     }
+// });
+
+// observer.observe(input, {
+//     attributes: true
+// });
